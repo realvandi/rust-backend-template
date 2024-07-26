11afde998926a85fc497027d38290753c297b412
Dylan Alvandi <g.dylanalvandi@gmail.com>
2024-07-26 17:58:25 +0200

    feat: Random number generator

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,31 @@
-use axum::{routing::get, Router};
+use axum::{extract::Query, response::Html, routing::get, Router};
+use rand::{thread_rng, Rng};
+use serde::Deserialize;
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-     // Route all requests on "/" endpoint to anonymous handler.
-    //
-    // A handler is an async function which returns something that implements
-    // `axum::response::IntoResponse`.
-
-    // A closure or a function can be used as handler.
     let app = Router::new().route("/", get(handler));
-    //        Router::new().route("/", get(|| async { "Hello, world!" }));
 
-    // Address that server will bind to.
-     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-
-    // Use `hyper::server::Server` which is re-exported through `axum::Server` to serve the app.
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
     axum::Server::bind(&addr)
-        // Hyper server takes a make service.
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
-async fn handler() -> &'static str {
-    "Hello, world!"
+// `Deserialize` need be implemented to use with `Query` extractor.
+#[derive(Deserialize)]
+struct RangeParameters {
+    start: usize,
+    end: usize,
+}
+
+async fn handler(Query(range): Query<RangeParameters>) -> Html<String> {
+    // Generate a random number in range parsed from query.
+    let random_number = thread_rng().gen_range(range.start..range.end);
+
+    // Send response in html format.
+    Html(format!("<h1>Random Number: {}</h1>", random_number))
 }
