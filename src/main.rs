f60ab8cb55a810e1b40c5860066b0b16d6820305
Dylan Alvandi <g.dylanalvandi@gmail.com>
2024-07-26 18:21:03 +0200

    feat: I have no idea what this commits

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ async fn main() {
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("listening on {}", addr);
-    axum::Server::bind(&addr)
+    axum::Server::bind(&addr)             
         .serve(app.into_make_service())
         .await
         .unwrap();
